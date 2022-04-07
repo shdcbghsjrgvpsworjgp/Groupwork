2cbcf1d91bf888d66eda997fe68223b1de063883
nian ye <102268848+shdcbghsjrgvpsworjgp@users.noreply.github.com>
2022-04-08 00:19:07 +0800

    Add files via upload

---
+++ Opretion.java
@@ -0,0 +1,11 @@
+public class Operation {
+    static int add(int a, int b){
+        int c = a + b;
+        return c;
+    }
+    static int sub(int a, int b){
+        int c = a - b;
+        return c;
+    }
+    
+}
